a60d0ce21910ae7505897367231e544b5f929923
Asheedkhan <94754248+ITzAsheed@users.noreply.github.com>
2026-04-09 16:52:46 -0400

    Fix typo in script purpose comment

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -2,7 +2,7 @@
 =====================================================================
 Create Database and Schemas
 =====================================================================
-Script Pu'rpose:
+Script Purpose:
     This script creates a new database named 'DataWarehouse' after checking if it already exists.
     If the database exits, it is dropped and recreated. Additionally, the script sets up three schemas 
     within the database: 'bronze', 'silver', and 'gold'.
